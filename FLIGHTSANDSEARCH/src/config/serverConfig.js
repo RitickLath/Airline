ede8cde1a8199e100b02f800f248ffd3f428feda
RitickLath <ritick943lath@gmail.com>
2024-05-02 09:40:12 +0530

    installed dependencies sequelize sequelize-cli mysql2 and initialize sequelize

--- FLIGHTSANDSEARCH/src/config/serverConfig.js
+++ FLIGHTSANDSEARCH/src/config/serverConfig.js
@@ -1,4 +1,6 @@
 // getting the port from .env file
+// we have not implemented these logics in index.js
+// as index.js is responsible for important code not environment variables 
 const dotenv = require("dotenv");
 // doing just the thing what do says
 dotenv.config();
--- FLIGHTSANDSEARCH/src/index.js
+++ FLIGHTSANDSEARCH/src/index.js
@@ -14,3 +14,6 @@ const setupAndStartServer = (async = () => {
 });
 
 setupAndStartServer();
+
+
+
